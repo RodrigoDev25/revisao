2accd01e492377302b01222b380db80bdefd4e44
RODRIGO INACIO DA SILVA <rodrigo.isilva1@senacsp.edu.br>
2025-08-15 21:06:34 -0300

    2

--- revisao.js
+++ revisao.js
@@ -1,8 +1,23 @@
 function soma() {
   const num1 = 20;
   const num2 = 15;
+  console.log("resultado da soma: ", num1 + num2)
+};
 
-  console.log(num1 + num2)
-}
+function multiplica() {
+    let num1 = 3;
+    let num2 = 5;
 
-soma()
+    let resultado = num1 * num2
+    console.log("resultado da multiplicação: ", resultado)
+};
+
+function subtrai(num1, num2) {
+    resultado = num1 - num2
+console.log("resultado da multiplicação: ", resultado)
+};
+
+soma();
+multiplica();
+subtrai(3, 1);
+subtrai(2, 1);
